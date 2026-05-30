53406ed5705ba3b1888cf37acea7a1fe6c6ff4cf
realferreira <ferreirajosepaulo@yahoo.com.br>
2026-05-30 19:20:05 -0300

    wip: current project progress

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import os
 
 TARGET_FOLDER = "test_folder"
 
+CATEGORIES = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".webp"],
+    "Documents": [".pdf", ".docx", ".txt"],
+    "Spreadsheets": [".xlsx", ".csv"],
+    "Videos": [".mp4", ".mov", ".avi"],
+    "Audios": [".mp3", ".wav"],
+    "Compressed": [".zip", ".rar", ".7z"],
+}
+
 print("File Organizer started!")
 
 files = os.listdir(TARGET_FOLDER)
@@ -9,4 +18,10 @@ files = os.listdir(TARGET_FOLDER)
 for file in files:
     file_name, file_extension = os.path.splitext(file)
 
-    print(f"File {file_name} | Extension: {file_extension}")
+category_found = "Others"
+
+for category, extensions in CATEGORIES.items():
+    if file_extension.lower() in extensions:
+        category_found = category
+
+        print(f"File: {file} | Category: {category_found}")
