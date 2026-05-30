af74946a861798884bea80b73bd0086c1c2872e7
José Ferreira <realjoseferreira@gmail.com>
2026-05-30 19:57:41 -0300

    refactor: organize code into functions

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ CATEGORIES = {
 }
 
 
+def get_category(file_extension):
+    for category, extensions in CATEGORIES.items():
+        if file_extension.lower() in extensions:
+            return category
+
+    return "Others"
+
+
 def get_unique_path(destination_path):
     if not os.path.exists(destination_path):
         return destination_path
@@ -33,32 +41,32 @@ def get_unique_path(destination_path):
         counter += 1
 
 
-print("File Organizer started!")
+def organize_files():
+    print("File Organizer started!")
 
-files = os.listdir(TARGET_FOLDER)
+    files = os.listdir(TARGET_FOLDER)
 
-for file in files:
-    file_path = os.path.join(TARGET_FOLDER, file)
+    for file in files:
+        file_path = os.path.join(TARGET_FOLDER, file)
 
-    if os.path.isdir(file_path):
-        continue
+        if os.path.isdir(file_path):
+            continue
 
-    file_name, file_extension = os.path.splitext(file)
+        file_name, file_extension = os.path.splitext(file)
 
-    category_found = "Others"
+        category_found = get_category(file_extension)
 
-    for category, extensions in CATEGORIES.items():
-        if file_extension.lower() in extensions:
-            category_found = category
+        category_folder = os.path.join(TARGET_FOLDER, category_found)
 
-    category_folder = os.path.join(TARGET_FOLDER, category_found)
+        if not os.path.exists(category_folder):
+            os.makedirs(category_folder)
 
-    if not os.path.exists(category_folder):
-        os.makedirs(category_folder)
+        destination_path = os.path.join(category_folder, file)
+        destination_path = get_unique_path(destination_path)
 
-    destination_path = os.path.join(category_folder, file)
-    destination_path = get_unique_path(destination_path)
+        shutil.move(file_path, destination_path)
 
-    shutil.move(file_path, destination_path)
+        print(f"Moved: {file} -> {category_found}")
 
-    print(f"Moved: {file} -> {category_found}")
+
+organize_files()
